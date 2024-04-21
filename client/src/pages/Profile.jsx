32f2db4e6ca7acae648117e4a439b82e8563c451
Shakti_Bhardwaj <shaktibhardwaj2030@gmail.com>
2024-04-21 18:22:53 +0530

    completed create listing page UI

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -9,6 +9,7 @@ import {
 import { app } from "../firebase";
 import {updateUserStart, updateUserFailure, updateUserSuccess, deleteUserFailure, deleteUserStart, deleteUserSuccess, signOutUserStart, signOutUserFailure, signOutUserSuccess} from '../redux/user/userSlice.js';
 import {useDispatch} from 'react-redux';
+import {Link} from 'react-router-dom';
 
 export default function Profile() {
   const fileRef = useRef(null);
@@ -166,6 +167,9 @@ export default function Profile() {
         <button disabled={loading} className="bg-slate-600 text-white rounded-lg p-3 uppercase hover:opacity-70 disabled:opacity-80">
           {loading? "Loading... ": " Update"}
         </button>
+        <Link className="bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-70" to={"/create-listing"}>
+          Create Listing
+        </Link>
       </form>
       <div className="flex justify-between mt-5">
         <span onClick={handleDeleteUser} className="text-red-700 cursor-pointer">Delete Account</span>
