0753f82ddb32aec7d90df1fded688fcc217476e7
Shakti_Bhardwaj <shaktibhardwaj2030@gmail.com>
2024-04-24 17:22:43 +0530

    added landlort functionality to the listing page

--- api/index.js
+++ api/index.js
@@ -31,6 +31,8 @@ app.use("/api/user", userRouter);
 app.use("/api/auth",authRouter);
 app.use("/api/listing",listingRouter);
 
+
+
 app.use((err, req, res, next)=>{
     const statusCode = err.statusCode || 500;
     const message = err.message || 'Internal Server Error';
