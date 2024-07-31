4797206acdeb5a15787c80d68dd08c92ada80a7b
Shakti_Bhardwaj <shaktibhardwaj2030@gmail.com>
2024-08-01 05:22:47 +0530

    Add footer

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+// components/Footer.js
+import React from "react";
+
+function Footer() {
+  return (
+    <footer style={{ textAlign: "center", padding: "1rem", background: "#f1f1f1" }}>
+      <p>&copy; Pinnacle Homes. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
